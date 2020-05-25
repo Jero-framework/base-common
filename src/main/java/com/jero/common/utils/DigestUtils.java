6aa46b6cf25ecd1b1b42d129f96845d36a5f6e56
zer0Black <lxtalx@gmail.com>
2020-05-25 11:26:16 +0800

    【修改加密类的名称】

---
+++ src/main/java/com/jero/common/utils/DigestUtils.java
@@ -0,0 +1,10 @@
+package com.jero.common.utils;
+
+/**
+ * @Description AES、SHA、MD5、RSA加解密算法
+ * @Author lixuetao
+ * @Date 2020/3/22
+ **/
+public class DigestUtils extends org.apache.commons.codec.digest.DigestUtils {
+
+}
